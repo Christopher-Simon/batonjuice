8b4c1ddf0fe4bab3198f00e7a7105295beb2c1f8
Christopher Simon <christopher.simon.wells@gmail.com>
2020-09-02 11:14:22 +0200

    Création de fonctions

--- Jeu Baton/main.c
+++ Jeu Baton/main.c
@@ -1,11 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void jeuIDA(int nombreDeBatons)
+    {
+        int batonsRetire;
+
+        printf("Je commence, je vais retirer 3 batons.\n");
+
+            nombreDeBatons = nombreDeBatons - 3;
+            printf("Il reste %d batons\n", nombreDeBatons);
+
+        while (nombreDeBatons > 0)
+        {
+            while (batonsRetire > 3 || batonsRetire <= 0)
+            {
+                printf("Combien de batons voulez vous retirer ? Retirez entre 1 et 3\n");
+                scanf("%d", &batonsRetire);
+                printf("Vous ne pouvez retirer qu'entre 1 et 3 batons\n");
+           }
+            nombreDeBatons = nombreDeBatons - batonsRetire;
+
+                 if (nombreDeBatons > 0)
+                 {
+                    batonsRetire = 4 - batonsRetire;
+                    printf("Je vais retirer %d batons\n", batonsRetire);
+                 }
+                 else
+                 {
+                     printf("Desole, vous avez perdu\n");
+                 }
+
+            nombreDeBatons = nombreDeBatons - batonsRetire;
+            };
+           printf("Want to replay ?\n");
+        }
+
 int main()
 {
-    int nombreDeBatons = 20;
     int batonsRetire = 0;
-    printf("Il y a %d batons\n", nombreDeBatons);
 
     char nomOrdi[] = "IDA";
     char prenom[100];
@@ -15,38 +47,20 @@ int main()
     scanf("%s", prenom);
     printf("Salut %s, pret pour une petite partie ?\n", prenom);
 
-    printf("Je commence, je vais retirer 3 batons.\n");
 
-    nombreDeBatons = nombreDeBatons - 3;
-
-
-    while (nombreDeBatons > 0)
-    {
-        printf("Il reste %d batons\n", nombreDeBatons);
-        printf("Combien de batons voulez vous retirer ? Retirez entre 1 et 3\n");
+    jeuIDA(20);
 
 
-        scanf("%d", &batonsRetire);
 
-        if (batonsRetire > 3 || batonsRetire <= 0)
-        {
-            printf("Vous ne pouvez retirer qu'entre 1 et 3 batons\n");
-            batonsRetire = 0;
-        }
-        nombreDeBatons = nombreDeBatons - batonsRetire;
 
-             if (nombreDeBatons > 0)
-             {
-                batonsRetire = 4 - batonsRetire;
-                printf("Je vais retirer %d batons\n", batonsRetire);
-             }
-             else
-             {
-                 printf("désolé, vous avez perdu\n");
-             }
-
-        nombreDeBatons = nombreDeBatons - batonsRetire;
-    }
+    //printf("Voulez-vous rejouer ? Rejouer : 1 Quitter : 0");
+    //scanf("%d", &rejouer);
+      //  if (rejouer > 1 || rejouer < 0)
+        //{
+          //  printf("Veuillez répondre par 1 ou 0\n");
+            //rejouer = 0;
+        //}
+    printf("Merci d'avoir joue");
 
     return 0;
 }
